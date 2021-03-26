e23df80de6bba90fba06d1e47352634d4e36a7a1
Hugh Chiou <hughchiou@gmail.com>
2021-03-26 08:25:35 +0000

    add OpenAPI

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module'
 
@@ -12,6 +13,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const globalPrefix = 'api'
   app.setGlobalPrefix(globalPrefix)
+  const config = new DocumentBuilder()
+    .setTitle('Port App API')
+    .setDescription('The Port App API description')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
   const port = process.env.PORT || 3333
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix)
